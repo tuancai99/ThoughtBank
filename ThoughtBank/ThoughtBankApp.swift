16011b6a65ed5ae96e31c7d5ab5e4d0f8f0388a1
Abdulaziz Albahar <abdulaziz@albahar.net>
2023-10-17 16:52:53 -0400

    Initial Commit

---
+++ ThoughtBank/ThoughtBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThoughtBankApp.swift
+//  ThoughtBank
+//
+//  Created by Abdulaziz Albahar on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ThoughtBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
